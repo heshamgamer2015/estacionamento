dbe11861f7633eb8e657e7e203116de4c435e830
Flash_zap_ <heshamgamer2015@gmail.com>
2023-05-10 19:47:59 -0400

    base

---
+++ src/main/java/br/com/uniamerica/estacinamento/EstacinamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.uniamerica.estacinamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacinamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstacinamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/uniamerica/estacinamento/EstacinamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uniamerica.estacinamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacinamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
